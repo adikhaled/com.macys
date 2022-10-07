e09549acb9001b785076c6fa7f2d3a20f9b61600
adikhaled <khldzmnd@gmail.com>
2022-10-06 21:05:26 -0400

    updated hw

--- src/main/java/com/macys/Base.java
+++ src/main/java/com/macys/Base.java
@@ -1,10 +1,14 @@
 package com.macys;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -15,12 +19,19 @@ public class Base {
 	
 	@BeforeMethod
 	public void setUp() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\khale\\eclipse-workspace\\com.macys\\driver\\chromedriver.exe");
-		driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+".\\driver\\chromedriver.exe");
+		//driver = new ChromeDriver();
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\khale\\eclipse-workspace\\com.macys\\driver\\geckodriver.exe");
+		//WebDriverManager.firefoxdriver().setup();
+		driver=new FirefoxDriver();
 		
-		driver.get("https://www.macys.com/");
+		//driver = new EdgeDriver();
+
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
+		driver.get("https://www.macys.com/");
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		
 	    homePage = new HomePage(driver);
 
